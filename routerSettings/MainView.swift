632c267ae0e5796f3a6b7265ee60e6d24f0cfeba
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-02-25 00:41:24 +0200

    i made ui changes

--- routerSettings/MainView.swift
+++ routerSettings/MainView.swift
@@ -9,21 +9,39 @@ import SwiftUI
 
 struct MainView: View {
     let routers = [
-        Router(name: "Router Model 1", tutorialYouTubeLink: "https://youtu.be/exampleLink1"),
-        Router(name: "Router Model 2", tutorialYouTubeLink: "https://youtu.be/exampleLink2")
+        Router(name: "Ruijie  RG-EW1200G PRO", tutorialYouTubeLink: "https://youtu.be/XH5fdCTmQU8"),
+        Router(name: "Tenda AC5 AC1200", tutorialYouTubeLink: "https://youtu.be/eGPME-raBU4")
     ]
+
+    @State private var searchText = ""
 
     var body: some View {
         NavigationView {
-            List(routers) { router in
+            List(routers.filter { router in
+                searchText.isEmpty || router.name.localizedCaseInsensitiveContains(searchText)
+            }) { router in
                 NavigationLink(destination: RouterDetailView(router: router)) {
                     Text(router.name)
+                        .fontWeight(.medium)
+                        .foregroundColor(.black) // Text color
+                        .padding() // Padding for the text
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color.red) // Item background color
+                        .cornerRadius(10) // Rounded corners
+                        .shadow(color: .gray.opacity(0.3), radius: 3, x: 0, y: 2) // Shadow for depth
+                        .padding(.vertical, 4) // Spacing between items
                 }
+                .listRowBackground(Color.orange) // Use this to set the background color for the entire row
+                .listRowInsets(EdgeInsets()) // Adjust or remove to match your design
             }
             .navigationTitle("Routers")
+            .searchable(text: $searchText)
+            .background(Color.orange) // Background color for the list/container
+            .listStyle(PlainListStyle()) // Style of the list
         }
     }
 }
+
 
 
 #Preview {
