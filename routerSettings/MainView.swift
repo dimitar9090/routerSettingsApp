f6b26024c00f84e6db79dbc0a786f010f9260159
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-03-12 00:34:17 +0200

    i add new router tenda w311r

--- routerSettings/MainView.swift
+++ routerSettings/MainView.swift
@@ -18,7 +18,8 @@ struct MainView: View {
         Router(name: "Tp-link C60", tutorialYouTubeLink: "https://youtu.be/1KcwsOCbI3E", image: "tp-linkc60"),
         Router(name: "Tenda AC6", tutorialYouTubeLink: "https://youtu.be/cEhdW0tdEpw", image: "Tendaac6"),
         Router(name: "Tenda AC10", tutorialYouTubeLink: "https://youtu.be/z_1MpOG7BJE", image: "tendaac10"),
-        Router(name: "Tp-link Archer C64", tutorialYouTubeLink: "https://youtu.be/Fb66skQXeo4", image: "tplinkarcherc64")
+        Router(name: "Tp-link Archer C64", tutorialYouTubeLink: "https://youtu.be/Fb66skQXeo4", image: "tplinkarcherc64"),
+        Router(name: "Tenda W311R", tutorialYouTubeLink: "https://youtu.be/qe9dTVNAsBo", image: "tendaw311r")
     ]
 
     let backgroundGradientMain = LinearGradient(gradient: Gradient(colors: [Color.orange, Color.pink]), startPoint: .leading, endPoint: .trailing)
