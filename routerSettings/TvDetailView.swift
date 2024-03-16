587aa007a4f6e42c069481be5e0ba0f4d4ac261d
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-03-17 00:44:44 +0200

    i fixed tv view and settings

---
+++ routerSettings/TvDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TvDetailView.swift
+//  routerSettings
+//
+//  Created by Dimitar Angelov on 17.03.24.
+//
+
+import SwiftUI
+
+struct TvDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TvDetailView()
+}
